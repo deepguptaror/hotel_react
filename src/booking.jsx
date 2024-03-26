330749df12ef9fb602855b01ad93d4bbc9494ea1
Deep Gupta <deepguptaror@gmail.com>
2024-03-26 16:27:34 +0530

    Add cancel functionality in booking component

--- src/booking.jsx
+++ src/booking.jsx
@@ -10,6 +10,7 @@ export class Bookings extends React.Component {
 
     // This binding is necessary to make `this` work in the callback
     this.bookingsList = this.bookingsList.bind(this);
+    this.cancelBooking = this.cancelBooking.bind(this);
     
   }
 
@@ -20,6 +21,7 @@ export class Bookings extends React.Component {
       <td>{booking.user_id}</td>
       <td>{booking.start_date}</td>
       <td>{booking.end_date}</td>
+      <td><a href='' data-id={booking.id} onClick={this.cancelBooking}>Cancel</a></td>
     </tr>
       
     );
@@ -27,6 +29,21 @@ export class Bookings extends React.Component {
     return (
       <tbody>{listItems}</tbody>
     );
+  }
+
+  cancelBooking(event) {
+
+    event.preventDefault();
+    const id = event.target.getAttribute('data-id')
+    fetch('http://127.0.0.1:4000/home/cancel_booking?id=' + id)
+      .then((res) => {
+        return res.json();
+      }).then((data) => {
+        console.log(data);
+        this.setState({bookings: data}); 
+      });
+
+
   }
 
   componentDidMount() {
@@ -49,6 +66,7 @@ export class Bookings extends React.Component {
             <th>User Name</th>
             <th>Start Date</th>
             <th>End date</th>
+            <th>Cancel Booking</th>
           </thead>
           
           {this.bookingsList(this.state.bookings)}
